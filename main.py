7f2cf76228c81a475daee4fb8c4a77a429bd9f51
Sam Lorimer <slorimerjr@gmail.com>
2022-04-11 19:38:36 +0000

    tested, seems to work

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Write your code below this line 👇
 #prime = only divisible by 1 and itself
-
-
-
+def prime_checker(number):
+    f = 2
+    s = []
+    while f < number:
+        if number % f == 0:
+            s.append(f)
+        f += 1
+    if len(s) > 0:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+            
 
 #Write your code above this line 👆
     
