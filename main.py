b47eab2601baeafe69f1fb8f240e4a5970a1c4b5
SamLorimer1 <slorimerjr@gmail.com>
2022-04-11 19:16:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write your code below this line 👇
+#prime = only divisible by 1 and itself
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
